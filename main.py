c974ae631040b41a58a6d60eebfcbdad8881370d
Joost <github@joostnieuweboer.nl>
2025-04-22 07:18:29 +0000

    Test version

--- main.py
+++ main.py
@@ -7,10 +7,34 @@ import pygame
 from constants import *
 
 def main():
+    # init the game
+    pygame.init()
+    # check if started
+    if pygame.get_init() == False: 
+        print(f"pygame failed to init!")
+        # stop the script
+        return
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # set the size and get the screen
+    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # infinite loop
+    while True:
+        #set surface to black
+        #screen.fill(BLACK)
+        pygame.Surface.fill(screen, BLACK)
+
+        if False:
+            break
+
+        # lastly: refresh the screen
+        pygame.display.flip()
+
+    return
 
 if __name__ == "__main__":
     main()
